c0c6600b0310df45b0e1d1c1f686dbb19b3f65d6
Marti-SV <a21marsalval@inspedralbes.cat>
2024-01-30 00:21:42 +0100

    Update Counter component styling and add Counter component to Header.astro

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 export function Counter () {
-    const [counter, setCounter] = useState(0);
+    let [counter, setCounter] = useState(0);
 
     return (
         <div className="text-xl">
-            <button onClick={() => setCounter(counter = counter + 1)} className="bg-green-300 w-10 py-1 rounded-3xl">+</button>
+            <button onClick={() => setCounter(counter = counter + 1)} className="bg-green-500 w-10 font-extrabold p-2 mx-2 rounded-3xl">+</button>
             <span>{counter}</span>
-            <button onClick={() => setCounter(counter = counter - 1)} className="bg-red-300 w-10 py-1 rounded-3xl">-</button>
+            <button onClick={() => setCounter(counter = counter - 1)} className="bg-red-500 w-10 font-extrabold p-2 mx-2 rounded-3xl">-</button>
         </div>
     )
 }
+
+export default Counter
