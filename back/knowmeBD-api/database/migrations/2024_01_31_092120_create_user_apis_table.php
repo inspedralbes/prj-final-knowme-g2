23c43ca91e3a98c91765f04aa719ee519ba6bb8e
julsluks <a22betvilver@inspedralbes.cat>
2024-01-31 11:46:29 +0100

    make model and migrations

---
+++ back/knowmeBD-api/database/migrations/2024_01_31_092120_create_user_apis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_api', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('userNickName')->unique();
+            $table->string('name');
+            $table->string('surnames');
+            $table->string('email')->unique();
+            $table->string('password');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_api');
+    }
+};
