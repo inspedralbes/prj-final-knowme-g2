4330a578304c3926738df7b4626dcdd05c1ac088
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2024-02-01 12:44:41 +0100

    Menu img funcional

---
+++ front/src/components/OptImg.jsx
@@ -0,0 +1,42 @@
+
+import { useState, useEffect } from 'react'
+import { counterStore } from "../stores/counterStore";
+export function OptImg({ selected, setSelected }) {
+    let img = undefined;
+    let [radius, setRadius] = useState(0);
+    let [border, setBorder] = useState(0);
+    let styles = {
+        borderRadius: radius + "px",
+        border: border + "px solid black"
+    }
+    useEffect(() => {
+        console.log(radius);
+    }, [radius]);
+    return (
+        <>
+            <h2>Image</h2>
+            <div>
+                <label htmlFor="imatge">+ Add File</label>
+                <input onInput={(event) => {
+                    var output = document.getElementById('preview');
+                    output.src = URL.createObjectURL(event.target.files[0])
+                }} className="hidden" type="file" name="img" id="imatge" />
+            </div>
+            <div><label htmlFor="radius" >Radius</label>
+                <input type="number" onInput={(event) => {
+                    setRadius(event.target.value);
+                }} className=" bg-black " value={radius} name="radius" id="radius" />
+            </div>
+
+            <label htmlFor="border" >Border</label>
+            <input type="number" onInput={(event) => {
+                setBorder(event.target.value);
+            }} className=" bg-black " value={border} name="border" id="border" />
+
+            <h2>Preview</h2>
+            <img alt="" style={styles} className={" border-2 border-solid "} id="preview" />
+        </>
+    )
+}
+
+export default OptImg
--- front/src/components/RightSideBar.jsx
+++ front/src/components/RightSideBar.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 
 import { MenuBar } from "./MenuBar.jsx";
 import { counterStore } from "../stores/counterStore";
-
+import { OptImg } from "./OptImg.jsx";
 export function RightSideBar() {
     let [selected, setSelected] = useState(null);
   
@@ -14,6 +14,8 @@ return (
         <div class="h-full w-96 bg-[#2d2d2d]   top-0  text-[#E8E9EA]">     
             <main class="mx-10 my-10">
             {selected == 0 ? <EditText />: null}
+            {selected == 3 ? <OptImg />: null}
+
             </main>
         </div>
         <nav class="h-full w-14 bg-[#4e4e4e] top-0  text-[#E8E9EA]">
