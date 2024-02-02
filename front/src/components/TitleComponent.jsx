b3aea3acc3216e70dba51da74b94f24b2a03a3d1
damiabreacervino <a21dambrecer@inspedralbes.cat>
2024-02-02 12:26:06 +0100

    Errores de merge

--- front/src/components/TitleComponent.jsx
+++ front/src/components/TitleComponent.jsx
@@ -4,7 +4,6 @@ export function TitleComponent({ componentData }) {
 
 
     return (
-
         <>
             <div className="w-2/3">
                 <h1 className={"text-8xl font-inter text-pretty " + (componentData.bold ? 'font-bold ' : '') + (componentData.italic ? 'italic ' : '')}> {componentData.text} </h1>
