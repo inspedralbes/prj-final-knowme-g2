e23d958000b9e009c769cdc2022a6c88e9138fa0
Marti-SV <a21marsalval@inspedralbes.cat>
2024-02-02 01:22:08 +0100

    class to className

--- front/src/components/RightSideBar.jsx
+++ front/src/components/RightSideBar.jsx
@@ -10,15 +10,15 @@ export function RightSideBar({ componentData, setComponentData }) {
     return (
 
         <>
-            <div class="h-screen w-1/4  min-w-fit bg-[#2d2d2d] flex text-[#E8E9EA]">
-                <div class="h-full w-96 bg-[#2d2d2d]  text-[#E8E9EA]">
-                    <main class="m-10">
+            <div className="h-screen w-1/4  min-w-fit bg-[#2d2d2d] flex text-[#E8E9EA]">
+                <div className="h-full w-96 bg-[#2d2d2d]  text-[#E8E9EA]">
+                    <main className="m-10">
                         {selected == 0 ? <EditText componentData={componentData} setComponentData={setComponentData} /> : null}
                         {selected == 3 ? <OptImg /> : null}
 
                     </main>
                 </div>
-                <nav class="h-full w-14 bg-[#4e4e4e] top-0  text-[#E8E9EA]">
+                <nav className="h-full w-14 bg-[#4e4e4e] top-0  text-[#E8E9EA]">
                     <MenuBar selected={selected} setSelected={setSelected} />
                 </nav>
             </div>
