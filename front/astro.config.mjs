2f442a630b04f91a5aa8557640f2e9a8ab475e71
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2024-02-05 08:53:48 +0100

    Tots els docker funcionen per a develop

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -5,5 +5,13 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+    server:{
+      // host: "0.0.0.0",
+      // hmr: { clientPort: 4321 },
+      // port: 4321, 
+      watch: { usePolling: true }
+    }
+  }
 });
