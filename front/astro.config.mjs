d46f6069bc08d09149f12b17e4c731eeb9d32683
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2024-02-05 10:55:14 +0100

    Intentat crear dockercompose a prod, no aconseguit

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,17 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   vite: {
-    server:{
+    server: {
       // host: "0.0.0.0",
       // hmr: { clientPort: 4321 },
       // port: 4321, 
-      watch: { usePolling: true }
+      watch: {
+        usePolling: true
+      }
     }
-  }
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
