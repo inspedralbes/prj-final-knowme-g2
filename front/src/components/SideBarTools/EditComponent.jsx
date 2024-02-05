eb75ab382120941699b8d7adc85b13c83690701a
damiabreacervino <a21dambrecer@inspedralbes.cat>
2024-02-05 12:39:52 +0100

    Style components

--- front/src/components/SideBarTools/EditComponent.jsx
+++ front/src/components/SideBarTools/EditComponent.jsx
@@ -18,7 +18,7 @@ export function EditComponent({ componentData, setComponentData }) {
         setPosY(evt.clientY);
     }
     const startDrag = (evt, id) => {
-       
+
     }
 
     const endDrag = (evt, id) => {
@@ -32,16 +32,28 @@ export function EditComponent({ componentData, setComponentData }) {
     }
     return (
         <>
-            <h2 className="text-2xl font-bold">Components</h2>
+            <h2 className="text-2xl font-bold mb-4">Components</h2>
             <div>
-                <div id="TitleComponent" className="bg-red-600" draggable onDragStart={(evt) => startDrag(evt, "TitleComponent")} onDragEnd={(evt) => endDrag(evt, "TitleComponent")}>
+                <div id="TitleComponent" className="p-2 rounded-md bg-slate-100 text-slate-900 h-32 mb-4 font-bold relative group"
+                    draggable onDragStart={(evt) => startDrag(evt, "TitleComponent")} onDragEnd={(evt) => endDrag(evt, "TitleComponent")}>
                     <h1>Title component</h1>
+                    <div className="transition-all opacity-0 rounded-md group-hover:opacity-100 absolute inset-0 bg-gray-400 bg-opacity-80 flex items-center justify-center">
+                        <span className="icon-[tabler--square-plus-2] size-6"></span>
+                    </div>
                 </div>
-                <div id="ImgComponent" className="bg-red-600 mt-4" draggable onDragStart={(evt) => startDrag(evt, "ImgComponent")} onDragEnd={(evt) => endDrag(evt, "ImgComponent")}>
+                <div id="ImgComponent" className="p-2 rounded-md bg-slate-100 text-slate-900 h-32 mb-4 font-bold relative group"
+                    draggable onDragStart={(evt) => startDrag(evt, "ImgComponent")} onDragEnd={(evt) => endDrag(evt, "ImgComponent")}>
                     <h1>Image</h1>
+                    <div className="transition-all opacity-0 rounded-md group-hover:opacity-100 absolute inset-0 bg-gray-400 bg-opacity-80 flex items-center justify-center">
+                        <span className="icon-[tabler--square-plus-2] size-6"></span>
+                    </div>
                 </div>
-                <div id="TitleComponent" className="bg-red-600 mt-4" draggable onDragStart={(evt) => startDragImg(evt, "SocialComponent")}>
+                <div id="TitleComponent" className="p-2 rounded-md bg-slate-100 text-slate-900 h-32 mb-4 font-bold relative group"
+                    draggable onDragStart={(evt) => startDrag(evt, "SocialComponent")}>
                     <h1>Social Media</h1>
+                    <div className="transition-all opacity-0 rounded-md group-hover:opacity-100 absolute inset-0 bg-gray-400 bg-opacity-80 flex items-center justify-center">
+                        <span className="icon-[tabler--square-plus-2] size-6"></span>
+                    </div>
                 </div>
             </div>
         </>
