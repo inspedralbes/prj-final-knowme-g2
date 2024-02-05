cd7e7f280bcc18fda1fd76f640067143fa141831
damiabreacervino <a21dambrecer@inspedralbes.cat>
2024-02-05 12:00:42 +0100

    EditImage style

--- front/src/components/SideBarTools/EditImage.jsx
+++ front/src/components/SideBarTools/EditImage.jsx
@@ -1,32 +1,60 @@
+import { useState, useEffect } from 'react';
+
 export function OptImg({ imgData, setImgData }) {
-    let img = undefined;
+    const [radiusImg, setRadiusImg] = useState(50);
+    const [borderImg, setBorderImg] = useState(1);
+
     let styles = {
-        borderRadius: imgData.radius + "px",
-        border: imgData.border + "px solid black"
+        borderRadius: radiusImg + "px",
+        border: borderImg + "px solid black"
     }
+    console.log(imgData.src);
+
+    const handleRangeChange = (e) => {
+        const nuevoValor = parseInt(e.target.value);
+        setRadiusImg(nuevoValor);
+    };
+
+    const handleBorderChange = (e) => {
+        const nuevoValor = parseInt(e.target.value);
+        setBorderImg(nuevoValor);
+    };
 
     return (
         <>
-            <h2 className="text-2xl font-bold mb-5">Image</h2>
-            <button className="relative font-bold text-lg w-full h-12 rounded-full bg-[#69b4ff] text-[#ffffff] hover:opacity-90 transition-all duration-100">
-                + Add File
-                <input onInput={(event) => {
-                    let output = document.getElementById('preview');
-                    output.src = URL.createObjectURL(event.target.files[0])
-                    setImgData({ ...imgData, src: output.src })
-                }} className=" top-0 left-0 rounded-full opacity-0 bg-slate-500 absolute w-full h-full hover:cursor-pointer" type="file" name="img" id="imatge" accept="image/*" />
-
-            </button>
-            <div><label htmlFor="radius" >Radius</label>
-                <input type="number" onInput={(e) => setImgData({ ...imgData, radius: e.target.value })}
-                    className=" bg-black " value={imgData.radius} name="radius" id="radius" />
+            <div className="h-8">
+                <h2 className="text-2xl font-bold mr-4 float-left">Image</h2>
+                <label htmlFor="imatge" className="relative float-right flex items-center justify-center font-bold text-md w-32 h-8 rounded-full bg-[#69b4ff] text-[#ffffff] hover:opacity-90 transition-all duration-100">
+                    + Add File
+                    <input onInput={(event) => {
+                        let output = document.getElementById('preview');
+                        output.src = URL.createObjectURL(event.target.files[0])
+                        setImgData({ ...imgData, src: output.src })
+                    }} className="top-0 left-0 rounded-full opacity-0 bg-slate-500 absolute w-full h-full hover:cursor-pointer" type="file" name="img" id="imatge" accept="image/*" />
+                </label>
             </div>
 
-            <label htmlFor="border" >Border</label>
-            <input type="number" onInput={(e) => setImgData({ ...imgData, border: e.target.value })} className=" bg-black " value={imgData.border} name="border" id="border" />
+            <div className="flex items-center justify-center gap-4 mb-4 mt-4">
+                <label htmlFor="radius" className="text-lg font-bold text-gray-100">Radius</label>
+                <input type="range" min="0" max="100" value={radiusImg} onChange={handleRangeChange}
+                    className="w-full h-1 opacity-70 transition-opacity duration-[0.2s] rounded-[5px]"
+                />
+                <p className="font-semibold">{radiusImg}%</p>
+            </div>
 
-            <h2>Preview</h2>
-            <img alt="" style={styles} className={" border-2 border-solid "} id="preview" />
+            <div className="mb-8 flex items-center">
+                <label htmlFor="border" className="text-lg font-bold text-gray-100 mr-2">Border</label>
+                <input type="number" min="0" max="100" value={borderImg} onChange={handleBorderChange} name="border" id="border"
+                    className="w-full p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
+                />
+            </div>
+
+
+            <h2 className="text-2xl font-bold mb-2">Preview</h2>
+            <img alt=""
+                style={styles}
+                className="border-2 border-solid rounded-md"
+                id="preview" />
         </>
     )
 }
