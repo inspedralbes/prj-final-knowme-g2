6e5d9459c9a7ee6615556e466922bb39f389f13d
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2024-02-07 12:12:20 +0100

    Arreglado error al no subir imagen que crasheaba

--- front/src/components/SideBarTools/EditImage.jsx
+++ front/src/components/SideBarTools/EditImage.jsx
@@ -7,6 +7,12 @@ export function EditImage() {
     const [img, setImg] = useState(null);
     const [variables, setVariables] = useState({ "zoom": 100, "rotate": 0, "border": 0, "radius": 0, "width": 250, "height": 250 });
     const [firstTime, setFirstTime] = useState(true);
+    let styles = {
+        border: `${content?.border}px solid`,
+        borderRadius: `${content?.radius}%`,
+        borderColor: 'white',
+    }
+        
     let editor = useRef(null);
     const handlerChange = (e) => {
         console.log(e);
@@ -34,21 +40,25 @@ export function EditImage() {
                     setImg(src);
                 }} className="top-0 left-0 rounded-full opacity-0 bg-slate-500 absolute size-full hover:cursor-pointer" type="file" name="img" id="imatge" accept="image/*" />
             </button>
-            <div className={(img ? "" : "hidden") + "flex align-middle justify-center flex-col"}>
+            <div className="flex mt-4"> 
+                <div className="flex items-center justify-center gap-4 mb-4 ">
+                    <label htmlFor="radius" className="text-lg text-gray-100">Radius</label>
+                    <input type="number" min="0" max="50" value={content?.radius} onChange={(e) => setContent({ ...content, radius: e.target.value })} name="border" id="border"
+                        className=" h-7 w-12 p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
+                    />
+                </div>
 
-                <AvatarEditor className=" max-w-64 max-h-64 ml-auto mr-auto border-2 border-gray-100 rounded-md overflow-hidden mt-4 mb-4 w-3/4 h-3/4"
-                    ref={editor}
-                    image={img}
-                    width={variables?.width}
-                    height={variables?.height}
-                    borderRadius={variables?.radius}
-                    border={variables?.border}
-                    color={[255, 255, 255, 0.6]} // RGBA
-                    scale={variables?.zoom / 100}
-                    rotate={variables?.rotate}
-                    onImageChange={handlerChange}
+                <div className="mb-4 ml-10 flex items-center">
+                    <label htmlFor="border" className="text-lg text-gray-100 mr-2">Border</label>
+                    <input type="number" min="0" max="100" value={content?.border} onChange={(e) => setContent({ ...content, border: e.target.value })} name="border" id="border"
+                        className=" h-7 w-12 p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
+                    />
+                </div>
+            </div>
+            {img != null ? 
+            <div className={"flex align-middle justify-center flex-col"}>
 
-                />
+
                 <div className="max-w-96">
                     <div className="flex items-center justify-center gap-4 mb-4 mt-4">
                         <label htmlFor="zoom" className="text-lg font-bold text-gray-100">Zoom</label>
@@ -77,31 +87,23 @@ export function EditImage() {
                             className="w-full p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
                         />
                     </div>
+                    <AvatarEditor style={styles} className=" max-w-64 max-h-64 ml-auto mr-auto border-2 border-gray-100 rounded-md overflow-hidden mt-4 mb-4 w-3/4 h-3/4"
+                        ref={editor}
+                        image={img}
+                        width={variables?.width}
+                        height={variables?.height}
+                        borderRadius={variables?.radius}
+                        border={variables?.border}
+                        color={[255, 255, 255, 0.6]} // RGBA
+                        scale={variables?.zoom / 100}
+                        rotate={variables?.rotate}
+                        onImageChange={handlerChange}
 
-                    <button onClick={() => {
-
-                        const canvas = editor.current.getImage();
-                        console.log(variables?.width);
-                        setContent({ ...content, src: canvas.toDataURL(), width: variables?.width, height: variables?.height })
-                    }
-                    }>Save</button>
+                    />
                 </div>
-            </div>
+            </div> : ""}
 
-            <div className="flex items-center justify-center gap-4 mb-4 mt-4">
-                <label htmlFor="radius" className="text-lg font-bold text-gray-100">Radius</label>
-                <input type="range" min="0" max="50" value={content?.radius} onChange={(e) => setContent({ ...content, radius: e.target.value })}
-                    className="w-full h-1  transition-opacity duration-[0.2s] rounded-[5px]"
-                />
-                <p className="font-semibold">{content?.radius}%</p>
-            </div>
 
-            <div className="mb-8 flex items-center">
-                <label htmlFor="border" className="text-lg font-bold text-gray-100 mr-2">Border</label>
-                <input type="number" min="0" max="100" value={content?.border} onChange={(e) => setContent({ ...content, border: e.target.value })} name="border" id="border"
-                    className="w-full p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
-                />
-            </div>
 
 
 
