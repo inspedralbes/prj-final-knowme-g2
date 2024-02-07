0a8a3a43f14650dc1725dcc76d34d993f483b8ef
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2024-02-07 11:48:45 +0100

    Input arreglado

--- front/src/components/SideBarTools/EditImage.jsx
+++ front/src/components/SideBarTools/EditImage.jsx
@@ -5,9 +5,23 @@ import AvatarEditor from 'react-avatar-editor'
 export function EditImage() {
     const { content, setContent } = useRightSideBarStore(state => state);
     const [img, setImg] = useState(null);
-    const [variables, setVariables] = useState({ "zoom": 100, "rotate": 0, "border": 0, "radius": 0, "width": 250, "height": 250});
+    const [variables, setVariables] = useState({ "zoom": 100, "rotate": 0, "border": 0, "radius": 0, "width": 250, "height": 250 });
+    const [firstTime, setFirstTime] = useState(true);
     let editor = useRef(null);
+    const handlerChange = (e) => {
+        console.log(e);
+        if (!firstTime) {
 
+            const canvas = editor.current.getImage();
+            setContent({ ...content, src: canvas.toDataURL(), width: variables?.width, height: variables?.height })
+        } else {
+            setFirstTime(false);
+            setTimeout(() => {
+                const canvas = editor.current.getImage();
+                setContent({ ...content, src: canvas.toDataURL(), width: variables?.width, height: variables?.height })
+            }, 1000);
+        }
+    }
 
     return (
         <>
@@ -20,8 +34,9 @@ export function EditImage() {
                     setImg(src);
                 }} className="top-0 left-0 rounded-full opacity-0 bg-slate-500 absolute size-full hover:cursor-pointer" type="file" name="img" id="imatge" accept="image/*" />
             </button>
-            <div className={img ? "" : "hidden"}>
-                <AvatarEditor
+            <div className={(img ? "" : "hidden") + "flex align-middle justify-center flex-col"}>
+
+                <AvatarEditor className=" max-w-64 max-h-64 ml-auto mr-auto border-2 border-gray-100 rounded-md overflow-hidden mt-4 mb-4 w-3/4 h-3/4"
                     ref={editor}
                     image={img}
                     width={variables?.width}
@@ -31,45 +46,47 @@ export function EditImage() {
                     color={[255, 255, 255, 0.6]} // RGBA
                     scale={variables?.zoom / 100}
                     rotate={variables?.rotate}
+                    onImageChange={handlerChange}
 
                 />
-                <div className="flex items-center justify-center gap-4 mb-4 mt-4">
-                    <label htmlFor="zoom" className="text-lg font-bold text-gray-100">Zoom</label>
-                    <input type="range" id='zoom' min="40" max="400" value={variables?.zoom} onChange={(e) => setVariables({ ...variables, zoom: e.target.value })}
-                        className=" w-full h-1 opacity-100 transition-opacity duration-[0.2s] rounded-[5px] "
-                    />
-                    <p className="font-semibold">{variables?.zoom}%</p>
-                </div>
-                <div className="flex items-center justify-center gap-4 mb-4 mt-4">
-                    <label htmlFor="rotate" className="text-lg font-bold text-gray-100">Rotate</label>
-                    <input type="range" id='rotate' min="0" max="360" value={variables?.rotate} onChange={(e) => setVariables({ ...variables, rotate: e.target.value })}
-                        className="w-full h-1 opacity-100 transition-opacity duration-[0.2s] rounded-[5px]"
-                    />
-                    <p className="font-semibold">{variables?.rotate}</p>
-                </div>
-                
-                <div className="flex items-center justify-center gap-4 mb-4 mt-4">
-                    <label htmlFor='width' className="text-lg font-bold text-gray-100">Width</label>
-                    <input type="number" min="0" max="500" value={variables?.width} onChange={(e) => setVariables({ ...variables, width: parseInt(e.target.value) })} name="width" id="width"
-                        className="w-full p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
-                    />
-                </div>
-                <div className="flex items-center justify-center gap-4 mb-4 mt-4">
-                    <label htmlFor='height' className="text-lg font-bold text-gray-100">Height</label>
-                    <input type="number" min="0" max="500" value={variables?.height} onChange={(e) => setVariables({ ...variables, height: parseInt(e.target.value) })} name="height" id="height"
-                        className="w-full p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
-                    />
-                </div>
-      
-                <button onClick={() => {
+                <div className="max-w-96">
+                    <div className="flex items-center justify-center gap-4 mb-4 mt-4">
+                        <label htmlFor="zoom" className="text-lg font-bold text-gray-100">Zoom</label>
+                        <input type="range" id='zoom' min="40" max="400" value={variables?.zoom} onChange={(e) => setVariables({ ...variables, zoom: e.target.value })}
+                            className=" w-full h-1 opacity-100 transition-opacity duration-[0.2s] rounded-[5px] "
+                        />
+                        <p className="font-semibold">{variables?.zoom}%</p>
+                    </div>
+                    <div className="flex items-center justify-center gap-4 mb-4 mt-4">
+                        <label htmlFor="rotate" className="text-lg font-bold text-gray-100">Rotate</label>
+                        <input type="range" id='rotate' min="0" max="360" value={variables?.rotate} onChange={(e) => setVariables({ ...variables, rotate: e.target.value })}
+                            className="w-full h-1 opacity-100 transition-opacity duration-[0.2s] rounded-[5px]"
+                        />
+                        <p className="font-semibold">{variables?.rotate}</p>
+                    </div>
 
-                    const canvas = editor.current.getImage();
-                    console.log(variables?.width);
-                    setContent({ ...content, src: canvas.toDataURL(), width: variables?.width, height: variables?.height })
-                }
-                }>Save</button>
+                    <div className="flex items-center justify-center gap-4 mb-4 mt-4">
+                        <label htmlFor='width' className="text-lg font-bold text-gray-100">Width</label>
+                        <input type="number" min="0" max="500" value={variables?.width} onChange={(e) => setVariables({ ...variables, width: parseInt(e.target.value) })} name="width" id="width"
+                            className="w-full p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
+                        />
+                    </div>
+                    <div className="flex items-center justify-center gap-4 mb-4 mt-4">
+                        <label htmlFor='height' className="text-lg font-bold text-gray-100">Height</label>
+                        <input type="number" min="0" max="500" value={variables?.height} onChange={(e) => setVariables({ ...variables, height: parseInt(e.target.value) })} name="height" id="height"
+                            className="w-full p-1 right-0 border rounded-md focus:outline-none text-right focus:border-blue-500 text-slate-700"
+                        />
+                    </div>
+
+                    <button onClick={() => {
+
+                        const canvas = editor.current.getImage();
+                        console.log(variables?.width);
+                        setContent({ ...content, src: canvas.toDataURL(), width: variables?.width, height: variables?.height })
+                    }
+                    }>Save</button>
+                </div>
             </div>
-
 
             <div className="flex items-center justify-center gap-4 mb-4 mt-4">
                 <label htmlFor="radius" className="text-lg font-bold text-gray-100">Radius</label>
@@ -86,10 +103,10 @@ export function EditImage() {
                 />
             </div>
 
-            
+
 
         </>
-        
+
     )
 }
 
