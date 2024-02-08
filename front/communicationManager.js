0970851a6d57ba91d59d5facecd07ed536d0257a
Arnau-fg <a22arnfergil@inspedralbes.cat>
2024-02-08 13:25:43 +0100

    rounting creat

---
+++ front/communicationManager.js
@@ -0,0 +1,13 @@
+
+export async function getProbes() {
+    const response = await fetch('http://localhost:1337/api/tests',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer a63cadd90d1cbc549966934103895d03cb2ea90f22ff04f3ae3eea150163a1c65d4159e598f280982988fa2733516489f58840ec9465d23a913d3594ccfeaf1a606719fb0e51c07dae7895e362609d05105d07cb72ca3471e7cd4a647c19708c4faffe9f26b597ab653de495815e1ece2252f7f6ca7a5054fc6e5ea4637d6a4b'
+        },
+
+    });
+    const data = await response.json();
+    return data.data;
+}
