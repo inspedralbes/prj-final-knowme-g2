23ce0517df5a2e6e15fab4329b2470ecbfc2b3a6
marti-sv <a21marsalval@inspedralbes.cat>
2024-02-13 09:10:18 +0100

    Refactor image and title components, and add save functionality

--- front/src/components/PortfolioComponents/ImgComponent.jsx
+++ front/src/components/PortfolioComponents/ImgComponent.jsx
@@ -4,9 +4,9 @@ import { useEffect } from 'react';
 export function ImgComponent({ id }) {
     const { setType, content, setContentIndex, addContent } = useRightSideBarStore(state => state);
     let contentIndex = content.findIndex(item => item.id === id);
-   
+
     useEffect(() => {
-        addContent({ ...content, src: 'https://via.placeholder.com/150', srcOrig: 'https://via.placeholder.com/150', border: 3, radius: 50, width: 250, height: 250, rotate: 0, zoom: 100, position : {x: 0.5, y:0.5}, align: 'left', id: id });
+        addContent({ src: 'https://via.placeholder.com/150', srcOrig: 'https://via.placeholder.com/150', border: 3, radius: 50, width: 250, height: 250, rotate: 0, zoom: 100, align: 'left', id: id });
     }, []);
 
     const handleClick = () => {
@@ -23,7 +23,11 @@ export function ImgComponent({ id }) {
     return (
 
         <>
-            <div onClick={() => handleClick()} className={"min-h-12 items-center flex " + (content[contentIndex]?.align == 'left' ? "justify-start" : content[contentIndex]?.align == 'center' ? "justify-center" : "justify-end")}>
+            <div onClick={() => handleClick()} className={"min-h-12 items-center flex " +
+                (content[contentIndex]?.align == 'left' ?
+                    "justify-start" : content[contentIndex]?.align == 'center' ?
+                        "justify-center" : content[contentIndex]?.align == 'right' ?
+                            "justify-end" : "")}>
                 <img className='size-52' alt="" style={styles} src={content[contentIndex]?.src} id="preview" />
             </div>
         </>
