d96f8b0d6d886f97d34162fee1de5d3f84e6ffda
Arnau-fg <a22arnfergil@inspedralbes.cat>
2024-02-14 09:16:16 +0100

    DOCKER funciona la connexió

--- front/src/components/ShowPortfolio.jsx
+++ front/src/components/ShowPortfolio.jsx
@@ -2,7 +2,8 @@ export function ShowPortfolio({ jsonData}) {
     return (
         <>
             <div className="h-screen w-5/6 max-w-proses mx-20 bg-white shadow-lg p-8 overflow-hidden overflow-ellipsis overflow-y-visible whitespace-nowrap">
-                {jsonData.attributes.example} {jsonData.attributes.proba}, you are number {jsonData.id}!
+                <h2>URL: {jsonData.domini.webURL}, el teu id_user: {jsonData.domini.id_user}, l'id del portfoli és: {jsonData.domini.id}!</h2>
+                <div>El teu contingut en string és: {jsonData.domini.content}</div>
             </div>
         </>
     )
--- front/src/services/communicationManager.js
+++ front/src/services/communicationManager.js
@@ -235,7 +235,7 @@ export function deleteDomain(token) {
 
 export function getDomains() {
     return new Promise((resolve, reject) => {
-        fetch('http://laravel:8000/api/domains', {
+        fetch('http://web/public/api/domains', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -257,7 +257,7 @@ export function getDomains() {
 
 export function showDomain(id) {
     return new Promise((resolve, reject) => {
-        fetch(`http://laravel:8000/api/domains/${id}`, {
+        fetch(`http://web/public/api/domains/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
