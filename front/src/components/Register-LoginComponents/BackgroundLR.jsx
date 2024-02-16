bb4aa66ba58532c313fdfb1cd23efe2d6ad49ed0
julsluks <a22betvilver@inspedralbes.cat>
2024-02-16 09:24:30 +0100

    update background register

--- front/src/components/Register-LoginComponents/BackgroundLR.jsx
+++ front/src/components/Register-LoginComponents/BackgroundLR.jsx
@@ -1,30 +1,42 @@
 export function BackgroundLR() {
     return (
-        // <div class="absolute top-0 left-0 w-full h-full overflow-hidden">
-        //     <div class="absolute top-0 left-0 w-full h-full">
-        //         <div class="h-full w-full bg-gradient-to-b from-blue-900 via-blue-700 to-purple-900"></div>
-        //     </div>
-        //     <div class="absolute top-0 left-0 w-full h-full">
-        //         <div class="cube bg-blue-500"></div>
-        //         <div class="sphere absolute top-1/4 left-1/4"></div>
-        //         <div class="sphere absolute top-1/2 right-1/4"></div>
-        //     </div>
-        // </div>
-
         <div class="absolute top-0 left-0 w-full h-full overflow-hidden">
             <div class="absolute top-0 left-0 w-full h-full">
                 <div class="h-full w-full bg-gradient-to-b from-blue-900 via-blue-700 to-purple-900"></div>
             </div>
             <div class="absolute top-0 left-0 w-full h-full">
-                <div class="absolute top-1/4 left-1/4 animate-ping">
-                    <div class="cube bg-blue-500 w-16 h-16"></div>
-                </div>
-                <div class="absolute top-1/2 right-1/4 animate-bounce">
+                {Array.from({ length: 10 }).map((_, index) => (
+                    <div class="absolute animate-ping"
+                        style={{
+                            top: `${Math.random() * 100}%`,
+                            left: `${Math.random() * 100}%`
+                            }}>
+                        <div class="cube bg-blue-500 w-16 h-16"></div>
+                    </div>
+                ))}
+                {Array.from({ length: 10 }).map((_, index) => (
+                    <div class="absolute animate-ping"
+                        style={{
+                            top: `${Math.random() * 100}%`,
+                            left: `${Math.random() * 100}%`
+                            }}>
+                        <div class="cube bg-blue-500 w-8 h-8"></div>
+                    </div>
+                ))}
+                {/* <div class="absolute animate-bounce"
+                    style={{
+                        top: `${Math.random() * 100}%`,
+                        left: `${Math.random() * 100}%`
+                        }}>
                     <div class="sphere bg-red-500 w-16 h-16 rounded-full"></div>
                 </div>
-                <div class="absolute top-1/2 left-1/4 animate-spin">
+                <div class="absolute animate-spin"
+                    style={{
+                        top: `${Math.random() * 100}%`,
+                        left: `${Math.random() * 100}%`
+                        }}>
                     <div class="sphere bg-red-500 w-16 h-16 rounded-full"></div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
